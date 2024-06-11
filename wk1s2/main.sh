51519968f2809b9d0fda79b16c6b69b26c311daf
saucekode <saucekoder@gmail.com>
2024-06-11 14:22:00 +0100

    added script to create s3 bucket with policies and main script

--- wk1s2/main.sh
+++ wk1s2/main.sh
@@ -1,1 +1,33 @@
 #!/bin/bash
+
+# Start aws CLI installation
+if [ "$1" == "--install" ]; then
+    ./install_aws_cli.sh
+    exit 0
+
+# Configure aws
+elif [ "$1" == "--configure" ]; then
+    ./aws_configure.sh
+    exit 0
+
+# Create an iam user
+elif [ "$1" == "--setup" ]; then
+    ./iam_user_setup.sh
+    exit 0
+
+# Create S3 bucker
+elif [ "$1" == "--bucket" ]; then
+    ./create_s3_bucket.sh
+    exit 0
+
+# Link bucket to an iam user
+elif [ "$1" == "--link" ]; then
+    ./awsS3_iam_link.sh
+    exit 0
+    
+else
+    echo "Mode not detected!"
+fi
+
+
+
